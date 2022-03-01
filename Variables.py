52ae6c5ec3357afc5a090af6d34705bdc13f80a6
Amr-shoeib <amr79.a@gmail.com>
2022-02-28 19:25:16 -0500

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,17 @@
+# this is a comment
+
+name = "Amr"
+age = "40"
+eyecolor = "brown"
+
+print("This is my first python script!!!")
+
+print(" My name is  + name ")
+
+name = input("What is your name?")
+
+print(" My name is now " + name)
+
+print("My age is " + str (age))
+
+
